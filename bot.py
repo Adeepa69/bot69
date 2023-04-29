b6c63d3fa4b8da6b88c34d048ed28eec5a5a3d2d
joysi <joysingh.js1@gmail.com>
2023-04-29 20:59:09 +1000

    github does protect me or not?

--- bot.py
+++ bot.py
@@ -5,9 +5,11 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+
 
 @client.event
 async def on_message(message):
@@ -17,4 +19,5 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run(input("Your token"))
+
+client.run("MTEwMTY1ODkzMzk0MjMwNDgwMA.Gm06Xc.5CzIEfb7T672Zn6BIfPvDlr5-akHFIwnXqwyNc")
