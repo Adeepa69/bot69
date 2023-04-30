4f7d84901ebf3206e126f7b00e0cf23c0125a02f
joysi <joysingh.js1@gmail.com>
2023-04-30 20:02:38 +1000

    message

--- bot.py
+++ bot.py
@@ -15,19 +15,20 @@ async def hello(interaction: discord, test: str):
     await interaction.response.send_message(test)
 
 
+@tree.command(name="clear", description="Clears the chat by how many messages you specify")
+async def clear(interaction: discord, amount: int):
+    # We need to validate the user's permissions that they have the manage_messages permission
+    if interaction.user.guild_permissions.manage_messages:
+        await interaction.channel.purge(limit=amount)
+        await interaction.response.send_message(f"Cleared {amount} messages", ephemeral=True)
+    else:
+        await interaction.response.send_message('fuck you', ephemeral=False)
+
+
 @client.event
 async def on_ready():
     await tree.sync()
     print(f'We have logged in as {client.user}')
-
-
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-#
-#     if message.content.startswith('$hello'):
-#         await message.reply('Hello!', mention_author=False)
 
 
 client.run(os.environ.get("Discord"))
