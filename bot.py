ce4cefab4c22007385b451998da58821df8aff72
Joy <29514446+fillwithjoy1@users.noreply.github.com>
2023-05-01 17:55:17 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from discord import app_commands
 from discord.ext import tasks
 from datetime import datetime, timedelta
 
+# Define normal Discord stuff
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -19,15 +20,15 @@ with open('server_policy', 'r') as file:
 
 print(server_policy)
 
-
+# Run this task every hour and run functions inside the main function below this decorator
 @tasks.loop(hours=1)
-# Write to server-policy file every hour
+# Write to server-policy file as backup
 async def backup_server_policy():
     with open('server_policy', 'w') as policy_file:
         policy_file.write(json.dumps(server_policy))
 
 
-# Clean up messages every hour
+# Clean up messages
 async def clear_messages():
     # Check server_policy for the message history of each server
     for server in server_policy:
@@ -39,6 +40,7 @@ async def clear_messages():
             await channel.purge(limit=10, check=check)
 
 
+# Check the message is older than the set amount of days
 def check(message: discord.Message):
     # Get the server object
     guild = message.guild
@@ -55,11 +57,13 @@ def check(message: discord.Message):
         return False
 
 
+# Simple slash command
 @tree.command(name="hello", description="Says hello to you")
 async def hello(interaction: discord, text: str):
     await interaction.response.send_message(text)
 
 
+# /clear command
 @tree.command(name="clear", description="Clears the chat by how many messages you specify")
 async def clear(interaction: discord, amount: int):
     # We need to validate the user's permissions that they have the manage_messages permission
@@ -86,6 +90,7 @@ async def history(interaction: discord, length: int):
         await interaction.user.timeout(expiration_time)
 
 
+# Runs as soon as the code is ready
 @client.event
 async def on_ready():
     await tree.sync()
